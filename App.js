cefb7bbf28756bcea2af47defdba495853a2cbe1
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-05-26 22:39:06 +0530

    create colors.js file and import in to app.js

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import colors from './Colors'
 
-export default function App() {
+export default class App extends React.Component {
+  render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <View style={{flexDirection: "row"}}>
+
+      <View style ={styles.divider}/>
+     </View>
     </View>
   );
 }
-
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +21,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  divider : {
+    backgroundColor : colors
+  }
+
+
 });
--- Colors.js
+++ Colors.js
@@ -1,5 +1,9 @@
 export default colors ={
     black : "#2D3436",
+    purple : "#582766",
+    lightpuple : "#9D41B7",
+    white : "#fffff",
+    
     
 
 }
